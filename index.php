a084f8368e6b0016c7428cc59e1fc026462d5c93
muzammil-hunani <42521921+muzammil-hunani@users.noreply.github.com>
2018-08-20 02:06:23 +0530

    save to json

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-echo 123;
+if(isset($_POST['pro_title']) && !empty($_POST['pro_title'])){
+  $data_array = array("title"=>$_POST['pro_title'], "location"=>$_POST['pro_location'], "client"=>$_POST['pro_client'], "sector"=>$_POST['pro_sector']);
+$inp = file_get_contents('db.json');
+$tempArray = json_decode($inp);
+array_push($tempArray, $data_array);
+$jsonData = json_encode($tempArray);
+file_put_contents('db.json', $jsonData);
+}
 ?>
