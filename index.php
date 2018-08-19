361583da4e97fb9ea9627c069de7e9902692dc2c
muzammil-hunani <42521921+muzammil-hunani@users.noreply.github.com>
2018-08-20 02:22:11 +0530

    update

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 if(isset($_POST['pro_title']) && !empty($_POST['pro_title'])){
   $data_array = array("title"=>$_POST['pro_title'], "location"=>$_POST['pro_location'], "client"=>$_POST['pro_client'], "sector"=>$_POST['pro_sector']);
-$inp = file_get_contents('db.json');
+$inp = file_get_contents('https://harlan-downstepper.herokuapp.com/db.json');
 $tempArray = json_decode($inp);
 array_push($tempArray, $data_array);
 $jsonData = json_encode($tempArray);
-file_put_contents('db.json', $jsonData);
-  $new_data = file_get_contents('db.json');
+file_put_contents('https://harlan-downstepper.herokuapp.com/db.json', $jsonData);
+  $new_data = file_get_contents('https://harlan-downstepper.herokuapp.com/db.json');
   print_r(json_decode($new_data));
 }
 ?>
